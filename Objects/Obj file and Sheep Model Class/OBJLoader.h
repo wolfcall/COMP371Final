1c9a21d5e0133c74f7e4837063e85b050b1e29b0
rstor <rstorm@gmail.com>
2015-03-21 21:47:56 -0400

    Created the OBJ Loader header file and moved the WormModel class into the Objects folder.

---
+++ Objects/Obj file and Sheep Model Class/OBJLoader.h
@@ -0,0 +1,26 @@
+// The following code was referenced from http://www.opengl-tutorial.org/
+
+#ifndef OBJLOADER_H
+#define OBJLOADER_H
+#include <vector>
+#include <glm/glm.hpp>
+using namespace glm;
+
+bool LoadOBJ(
+	const char * path,
+	std::vector<glm::vec3> & outVertices,
+	std::vector<glm::vec2> & outUvs,
+	std::vector<glm::vec3> & outNormals
+	);
+
+
+
+bool LoadAssImp(
+	const char * path,
+	std::vector<unsigned short> & indices,
+	std::vector<glm::vec3> & vertices,
+	std::vector<glm::vec2> & uvs,
+	std::vector<glm::vec3> & normals
+	);
+
+#endif
